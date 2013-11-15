9f52d071f60255f32d7e5554b0cac0d78430c618
Ben Johnson <benbjohnson@yahoo.com>
2013-11-14 20:05:08 -0500

    Move raft js under raft.

--- raft/js/index.js
+++ raft/js/index.js
@@ -3,7 +3,7 @@
 /*jslint browser: true, nomen: true*/
 /*global $, define, d3, playback*/
 
-define(["./model", "./layout", "./frames/init", "../domReady!"], function (Model, Layout, frames, doc) {
+define(["./model", "./layout", "./frames/init", "../../js/domReady!"], function (Model, Layout, frames, doc) {
     var player = playback.player();
     player.layout(new Layout("#chart"));
     player.model(new Model());
